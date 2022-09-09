3e28080b14fddb9b7048593ae0be21380a1f2bfc
RojasLuciano <lucianorojas@uala.com>
2022-09-08 22:55:57 -0300

    Configurado el build y deploy a firebase

--- TP-LABO4/TP-SALA-DE-JUEGOS/src/environments/environment.prod.ts
+++ TP-LABO4/TP-SALA-DE-JUEGOS/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBno1uXJw6birRZgQFOKOgMs3CWo3shdYk",
+    authDomain: "labo4-241ef.firebaseapp.com",
+    projectId: "labo4-241ef",
+    storageBucket: "labo4-241ef.appspot.com",
+    messagingSenderId: "452483955767",
+    appId: "1:452483955767:web:cfe13408823c63659c93b2",
+    measurementId: "G-MG7RC5CTNL"
+  }
 };
--- TP-LABO4/TP-SALA-DE-JUEGOS/src/environments/environment.ts
+++ TP-LABO4/TP-SALA-DE-JUEGOS/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig : {
     apiKey: "AIzaSyBno1uXJw6birRZgQFOKOgMs3CWo3shdYk",
     authDomain: "labo4-241ef.firebaseapp.com",
